b0dd29c240a585135fa8a9d614c696acd412e138
Ivan Yastrebov <esim.i2p@gmail.com>
2021-12-19 22:44:28 +0300

    GNU/Linux, Вс 19 дек 2021 22:44:28 MSK

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -3,5 +3,4 @@ export default [
   require("/home/sss/my-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/home/sss/my-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
   require("/home/sss/my-docs/src/css/custom.css"),
-  require("/home/sss/my-docs/node_modules/@docusaurus/plugin-pwa/src/registerSw.js"),
 ];
